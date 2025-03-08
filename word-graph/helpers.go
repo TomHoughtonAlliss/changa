730725b3324c2e69faf867eee9949ec4ac78d1e4
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-03-08 02:21:25 +0000

    Added merge and comments

--- word-graph/helpers.go
+++ word-graph/helpers.go
@@ -1,5 +1,8 @@
 package wordgraph
 
+// oneAway iterates over two strings and compares characters.
+//
+// It returns true if exactly one character differs between the two strings.
 func oneAway(a, b string) bool {
 	difs := 0
 
@@ -10,4 +13,13 @@ func oneAway(a, b string) bool {
 	}
 
 	return difs == 1
+}
+
+// merge combines the keys and values of two maps, and returns the result.
+func merge(m, n map[string]any) map[string]any {
+	for k, v := range n {
+		m[k] = v
+	}
+
+	return m
 }
